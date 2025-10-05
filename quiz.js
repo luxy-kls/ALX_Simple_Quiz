61641b013ea10e38db70c1048b73a3c89057e408
Keyman Luxy Sackey <sackeykeyman@gmail.com>
2025-10-05 13:49:34 +0100

    Created a simple interative quiz

---
+++ quiz.js
@@ -0,0 +1,18 @@
+function checkAnswer(){
+let correctAnswer = "4";
+document.querySelector('input[name="quiz"]:checked');
+let userAnswer = selectedOption ? selectedOption.value : null; 
+
+let feedback = document.getElementById("feedback");
+
+//comparing answers
+if(userAnswer === correctAnswer){
+     feedback.textContent = "Correct! Well done.";
+} else{
+    feedback.textContent = "That's incorrect. Try again!";
+}
+}
+
+
+//adding event listener to submit button
+document.getElementById("submit-answer").addEventListener("click", checkAnswer);
